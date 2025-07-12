d8c6b4fdee0241772ba133214aee38c6ef4d26bf
Ajay <128794124+Ajaypanchal4326@users.noreply.github.com>
2025-07-12 18:16:26 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductDetail from "./pages/ProductDetail";
-import CartPage from "./pages/CartPage"; // ✅ You'll add this page
-import { CartProvider } from "./context/cartcontext"; // ✅ Create this file
+import CartPage from "./pages/CartPage";
+import { CartProvider } from "./context/cartcontext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/product/:barcode" element={<ProductDetail />} />
-          <Route path="/cart" element={<CartPage />} /> {/* ✅ New Cart Page */}
+          <Route path="/cart" element={<CartPage />} />
         </Routes>
       </BrowserRouter>
     </CartProvider>
